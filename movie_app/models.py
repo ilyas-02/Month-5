f1445b905457c5930de7cb3cb93a31ef6f7c006e
ilyas-02 <100939446+ilyas-02@users.noreply.github.com>
2022-06-21 13:07:15 +0500

    HW_2

--- movie_app/models.py
+++ movie_app/models.py
@@ -12,13 +12,38 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     duration = models.FloatField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE,
+                                 related_name='movies_count')
 
     def __str__(self):
         return self.title
 
+    @property
+    def rating(self):
+        reviews = self.reviews.all()
+        if not reviews:
+            return 0
+        average = 0
+        for i in reviews:
+            average += i.stars
+        return average / reviews.count()
+
+
+STAR_CHOICES = (
+    (1, '*'),
+    (2, '* *'),
+    (3, '* * *'),
+    (4, '* * * *'),
+    (5, '* * * * *')
+)
+
 
 class Review(models.Model):
     text = models.TextField(null=True, blank=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='reviews')
+    stars = models.IntegerField(default=1, choices=STAR_CHOICES)
+
+    def __str__(self):
+        return self.text
 # Create your models here.
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -2,22 +2,24 @@ from rest_framework import serializers
 from movie_app.models import Director, Movie, Review
 
 
-class DirectorListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Director
-        fields = '__all__'
-
-
 class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
 
 
+class DirectorListSerializer(serializers.ModelSerializer):
+    movies_count = MovieListSerializer(many=True)
+
+    class Meta:
+        model = Director
+        fields = 'id name movies_count'.split()
+
+
 class ReviewListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = 'id text stars'.split()
 
 
 class DirectorDetailSerializer(serializers.ModelSerializer):
@@ -27,9 +29,11 @@ class DirectorDetailSerializer(serializers.ModelSerializer):
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
+    reviews = ReviewListSerializer(many=True)
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = 'id rating title description duration reviews'.split()
 
 
 class ReviewDetailSerializer(serializers.ModelSerializer):
